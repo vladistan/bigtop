d5b2568b05e4aa0f49d53f5fd2325ca5959eaf7d
Vlad Korolev <vlad@v-lad.org>
2023-12-30 18:17:43 -0500

    use pyenvs python 2

--- ci/build.sh
+++ ci/build.sh
@@ -4,12 +4,15 @@ set -e
 
 
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0/
-export PATH=$PATH:/usr/local/apache-maven-3.8.8/bin
+export PATH=$PATH:/usr/local/apache-maven-3.8.8/bin:/usr/local/pyenv/bin:/var/go/.pyenv/versions/2.7.18/bin
 
 echo "JOB: ${GO_JOB_NAME}"
 
 which mvn
 mvn --version
+
+python2 --version
+
 
 ./gradlew "${GO_JOB_NAME}" -Dbuildwithdeps=true -PparentDir=/usr/bigtop -PpkgSuffix
 
